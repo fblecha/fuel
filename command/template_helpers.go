2c2b78ce3ee232e05c8e8d24ba6ff4afab7504bd
Frank Blecha <frank@blecha.org>
2015-10-25 20:56:51 -0600

    trying something for CI

--- command/template_helpers.go
+++ command/template_helpers.go
@@ -28,7 +28,11 @@ func LoadPartialTemplates(appDir string, partialTemplatePaths []string, collecto
 		}
 
 		//HACK revisit using template.Must
-		collectorTemplate = template.Must(collectorTemplate.Clone())
+		collectorTemplate, err = collectorTemplate.Clone()
+		if err != nil {
+			log.Printf("Error was %s \n", err)
+			return collectorTemplate
+		}
 		name := ConvertTemplateName(appDir, path)
 		//fmt.Println(input)
 		fmt.Printf("templateName = %s \n", name)
